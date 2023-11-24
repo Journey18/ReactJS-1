caf9321dab04c8ec0cde1f93d7dd9786a760dcf2
Journey18 <nghia.nguyentrung18@gmail.com>
2023-11-24 12:11:49 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BaiTapThucHanhLayOut from "./component/BaiTapThucHanhLayOut/BaiTapThucHanhLayOut";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapThucHanhLayOut></BaiTapThucHanhLayOut>
+    </div>
+  );
+}
+
+export default App;
